41b4076f74352d19d07da9b4afa4f0b262f4e272
maisamer <maisamir896@gmail.com>
2019-06-03 00:03:48 +0200

    order data

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function rendeCafe(doc){
     })
 }
 
-db.collection('coffee').where('city','>=',"helwan").get().then((snapshot)=>{
+db.collection('coffee').where('city','==',"helwan").orderBy('name').get().then((snapshot)=>{
     snapshot.docs.forEach(element => {
         rendeCafe(element);
     });
