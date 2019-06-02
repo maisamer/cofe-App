e0860ff925531174b55d9424f3cabbd008ad8ac2
maisamer <maisamir896@gmail.com>
2019-06-02 23:32:55 +0200

    delete item in firestore

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ function rendeCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('data-id',doc.id);
     name.textContent=doc.data().name;
     city.textContent=doc.data().city;
+    cross.textContent='x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
+
+    // deleting data
+    cross.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id'); // get id fton li
+        db.collection('coffee').doc(id).delete();
+    })
 }
 
 db.collection('coffee').get().then((snapshot)=>{
